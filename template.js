57f546b60882c8110ea634b9e662527b24e609eb
Alex Renard <prioxisfx@gmail.com>
2024-01-13 13:16:57 -0600

    Added template for javascript file so I make sure to include a header with my information

---
+++ template.js
@@ -0,0 +1,11 @@
+/*  JavaScript 7th Edition
+    Chapter x
+    Assignment name
+
+    Author: William Renard
+    Date: xx-xx-2024
+
+    Filename: xx
+*/
+
+//define variables for service name and service speed

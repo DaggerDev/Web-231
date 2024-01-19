29beabf18b827f07b7ef1f0cd13598f86739f460
Alex Renard <prioxisfx@gmail.com>
2024-01-19 14:03:44 -0600

    Completed week 2 assignment

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,27 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: William Renard
+      Date: 1-19-2024
 
       Filename: project02-02.js
  */
- 
+
+function verifyForm() {
+
+  var name = document.getElementById('name').value;
+
+  var email = document.getElementById('email').value;
+
+  var phone = document.getElementById('phone').value;
+
+  if (name === '' || email === '' || phone === '') {
+    window.alert('Please fill in all fields.');
+  }
+
+  if (name !== '' && email !== '' && phone !== '') {
+    window.alert('Thank you!');
+  }
+}
+
+document.getElementById('submit').onclick = verifyForm;
